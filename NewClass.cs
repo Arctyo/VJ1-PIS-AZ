527d8c63fdae8a8fcec6408d68fa116e822fcbcb
azuvel <azuvel00@fesb.hr>
2025-03-17 20:04:18 +0100

    Added new class

---
+++ NewClass.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VJ1_PIS_AZ
+{
+    internal class Class1
+    {
+    }
+}
