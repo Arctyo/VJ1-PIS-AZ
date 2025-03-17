e409c634e14a68a2909193dcbfab978148d378b0
student <student@B420-06>
2025-03-17 19:43:38 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VJ1_PIS_AZ
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
